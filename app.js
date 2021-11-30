b2d983df72e1de323143fdcfae12081ca83d93e9
Vika Trofimova <70720521+Lenivaiavi@users.noreply.github.com>
2021-11-30 13:06:23 +0300

    add files

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#FFB6C1', '#9370DB', '#F4A460', '#A52A2A', '#778899', '#7FFFD4']
+const SQUARES_NUMBER = 2500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor (square))
+
+    square.addEventListener('mouseleave', () => removeColor (square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors [index]
+}
